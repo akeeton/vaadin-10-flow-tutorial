8b17e8644dbd784dec3f4df872c512532ccff03f
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-05-28 15:33:21 -0400

    Add filter for customer names

--- src/main/java/com/vaadin/MainView.java
+++ src/main/java/com/vaadin/MainView.java
@@ -1,8 +1,14 @@
 package com.vaadin;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcons;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -14,20 +20,30 @@ public class MainView extends VerticalLayout {
 
     private CustomerService service = CustomerService.getInstance();
     private Grid<Customer> grid = new Grid<>();
+    private TextField filterText = new TextField();
 
     public MainView() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setValueChangeMode(ValueChangeMode.EAGER);
+        filterText.addValueChangeListener(e -> updateList());
+
+        Button clearFilterTextBtn = new Button(new Icon(VaadinIcons.CLOSE_CIRCLE));
+        clearFilterTextBtn.addClickListener(e -> filterText.clear());
+
+        HorizontalLayout filtering = new HorizontalLayout(filterText, clearFilterTextBtn);
+
         grid.setSizeFull();
 
         grid.addColumn(Customer::getFirstName).setHeader("First name");
         grid.addColumn(Customer::getLastName).setHeader("Last name");
         grid.addColumn(Customer::getStatus).setHeader("Status");
 
-        add(grid);
+        add(filtering, grid);
         setHeight("100vh");
         updateList();
     }
 
     public void updateList() {
-        grid.setItems(service.findAll());
+        grid.setItems(service.findAll(filterText.getValue()));
     }
 }
