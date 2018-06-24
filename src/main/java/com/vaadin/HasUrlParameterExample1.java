3b6018b5163ef7684717687d12ac13f549014b3d
Andrew Keeton <208120+akeeton@users.noreply.github.com>
2018-06-24 17:16:24 -0400

    Try @WildcardParameter

--- src/main/java/com/vaadin/HasUrlParameterExample1.java
+++ src/main/java/com/vaadin/HasUrlParameterExample1.java
@@ -4,11 +4,12 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.WildcardParameter;
 
 @Route("greet")
 public class HasUrlParameterExample1 extends Div implements HasUrlParameter<String> {
     @Override
-    public void setParameter(BeforeEvent beforeEvent, String s) {
+    public void setParameter(BeforeEvent beforeEvent, @WildcardParameter String s) {
         setText(String.format("Hello, %s!", s));
     }
 }
